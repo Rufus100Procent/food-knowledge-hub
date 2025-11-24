26a1cf6cbf83e184a0c3a2a4fa65f319760fe544
stykle <Maxamadabdulahist@outlook.com>
2025-11-24 13:27:02 +0100

    update

--- src/test/java/com/example/foodknowledgehub/AbstractPostgresContainer.java
+++ src/test/java/com/example/foodknowledgehub/AbstractPostgresContainer.java
@@ -7,7 +7,7 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
-public abstract class AbstractPostgresContainerTest {
+public abstract class AbstractPostgresContainer {
 
     @Container
     protected static final PostgreSQLContainer<?> POSTGRES =
--- src/test/java/com/example/foodknowledgehub/MainTest.java
+++ src/test/java/com/example/foodknowledgehub/MainTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //@SpringBootTest
-class MainTests {
+class MainTest {
 
     @Test
     void contextLoads() {

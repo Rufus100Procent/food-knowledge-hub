a69031fc4eae5db9d847241af879da4ac99f4401
stykle <Maxamadabdulahist@outlook.com>
2025-11-24 12:31:10 +0100

    added CRUD / validation dto / SecurityConfig

--- src/main/java/com/example/foodknowledgehub/modal/MacromineralAmount.java
+++ src/main/java/com/example/foodknowledgehub/modal/MacromineralAmount.java
@@ -1,7 +1,48 @@
 package com.example.foodknowledgehub.modal;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+@Embeddable
 public class MacromineralAmount {
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "macromineral")
     private Macromineral macromineral;
+
+    @Column(name = "amount_mg")
     private double amountMilligrams;
+
+    @Column(name = "daily_value_percent")
     private double dailyValuePercent;
+
+    //<editor-fold desc="Getters and Setters">
+
+    public Macromineral getMacromineral() {
+        return macromineral;
+    }
+
+    public void setMacromineral(Macromineral macromineral) {
+        this.macromineral = macromineral;
+    }
+
+    public double getAmountMilligrams() {
+        return amountMilligrams;
+    }
+
+    public void setAmountMilligrams(double amountMilligrams) {
+        this.amountMilligrams = amountMilligrams;
+    }
+
+    public double getDailyValuePercent() {
+        return dailyValuePercent;
+    }
+
+    public void setDailyValuePercent(double dailyValuePercent) {
+        this.dailyValuePercent = dailyValuePercent;
+    }
+    //</editor-fold>
 }
+

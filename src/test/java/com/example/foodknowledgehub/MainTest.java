5917a3bdc6294145301b183817a5258bb8ed809f
stykle <Maxamadabdulahist@outlook.com>
2025-12-24 15:45:05 +0100

    update

--- src/test/java/com/example/foodknowledgehub/MainTest.java
+++ src/test/java/com/example/foodknowledgehub/MainTest.java
@@ -2,14 +2,17 @@ package com.example.foodknowledgehub;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 //@SpringBootTest
 class MainTest {
 
     @Test
     void contextLoads() {
-        assertFalse(false, "hello-world");
+
+        String test1  = "a".repeat(10);
+
+        assertEquals(10, test1.length());
     }
 
 }

2ee97c37f7c55ca69a8630504f10d29008e99b0d
stykle <Maxamadabdulahist@outlook.com>
2026-01-09 18:58:30 +0100

    commit

--- src/main/java/com/example/foodknowledgehub/Main.java
+++ src/main/java/com/example/foodknowledgehub/Main.java
@@ -10,4 +10,15 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
+    // List to do:
+    /*
+    * Image for micro and vitamin
+    * bulk api for vitamin, micro and macro
+    * unit test for micro, macro and vitamin service
+    * unit test for micro, macro and vitamin API's
+    * Login lock wall before  viewing full data / first 5 rows free
+    * pagination / loading
+    * rate limiting
+    * improve image loading, current problem data + image loads longer
+     * */
 }

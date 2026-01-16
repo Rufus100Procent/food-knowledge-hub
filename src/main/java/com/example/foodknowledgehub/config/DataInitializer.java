1e9fb2814860379959973a7a86c8aeac52b0a664
stykle <Maxamadabdulahist@outlook.com>
2026-01-16 10:50:41 +0100

    data init for quick bulk insert

---
+++ src/main/java/com/example/foodknowledgehub/config/DataInitializer.java
@@ -0,0 +1,36 @@
+package com.example.foodknowledgehub.config;
+
+import com.example.foodknowledgehub.service.MacromineralInfoService;
+import com.example.foodknowledgehub.service.MicromineralInfoService;
+import com.example.foodknowledgehub.service.VitaminInfoService;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Profile("!test")
+@Transactional
+@Component
+public class DataInitializer implements ApplicationRunner {
+
+    private final MacromineralInfoService macromineralInfoService;
+    private final MicromineralInfoService micromineralInfoService;
+    private final VitaminInfoService vitaminInfoService;
+
+    public DataInitializer(MacromineralInfoService macromineralInfoService, MicromineralInfoService micromineralInfoService, VitaminInfoService vitaminInfoService) {
+        this.macromineralInfoService = macromineralInfoService;
+        this.micromineralInfoService = micromineralInfoService;
+        this.vitaminInfoService = vitaminInfoService;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+//        macromineralInfoService.createBulkInsert();
+//        micromineralInfoService.createBulkInsert();
+//        vitaminInfoService.createBulkInsert();
+
+        System.out.println("hello");
+    }
+}

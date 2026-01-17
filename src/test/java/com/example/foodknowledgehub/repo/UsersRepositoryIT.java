926e227b2c7d8a22d88cee6b858465b92d0e68f0
stykle <Maxamadabdulahist@outlook.com>
2026-01-17 11:26:56 +0100

    added IT for UsersRepo

---
+++ src/test/java/com/example/foodknowledgehub/repo/UsersRepositoryIT.java
@@ -0,0 +1,36 @@
+package com.example.foodknowledgehub.repo;
+
+import com.example.foodknowledgehub.helper.AbstractPostgresContainer;
+import com.example.foodknowledgehub.security.user.Role;
+import com.example.foodknowledgehub.security.user.modal.Users;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+class UsersRepositoryIT extends AbstractPostgresContainer {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Test
+    void shouldFindUserByUsername() {
+        Users info = new Users();
+        info.setUserName("John");
+        info.setRole(Role.ADMIN_ROLE);
+        info.setPasswordHash("8jXoaX3omqCF+0ie3dIjTsTY/qbVKgMFH07rGAITeoE=");
+        usersRepository.save(info);
+
+        Optional<Users> result =
+                usersRepository.findByUserName(info.getUserName());
+
+        assertTrue(result.isPresent());
+        assertEquals(info.getUserName(), result.get().getUserName());
+        assertEquals(Role.ADMIN_ROLE, result.get().getRole());
+    }
+}
